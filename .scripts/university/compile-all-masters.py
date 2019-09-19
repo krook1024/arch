d539d6269871a7297dd5d9cc30136a153723ae8d
krook1024 <krook1024@gmail.com>
2019-09-19 22:19:08 +0200

    ⬆ Update dotfiles (📅 2019-09-19 22:19:08) 👉

---
+++ .scripts/university/compile-all-masters.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+from courses import Courses
+
+for course in Courses():
+    try:
+        lectures = course.lectures
+
+        r = lectures.parse_range_string('all')
+        lectures.update_lectures_in_master(r)
+        lectures.compile_master()
+    except:
+        continue
